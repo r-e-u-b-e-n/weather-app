9232d4d16b262f508ab16ff1cac819d9e336e458
Reuben <dsouzareuben79@gmail.com>
2025-07-14 15:35:11 +0530

    packages

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/png/homescreen.png'),
+            fit: BoxFit.cover
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/themes/themes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightPrimary = Color(0xFF000000);
+  static const Color lightSecondary = Color(0xFF3C3C43);
+  static const Color lightTertiary = Color(0xFF3C3C43);
+  static const Color lightQuaternary = Color(0xFF3C3C43);
+
+  static const Color darkPrimary = Color(0xFFFFFFFF);
+  static const Color darkSecondary = Color(0xFFEBEBF5);
+  static const Color darkTertiary = Color(0xFFEBEBF5);
+  static const Color darkQuaternary = Color(0xFFEBEBF5);
+}
