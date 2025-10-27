3229cbdf201ec3939dfaff43f3976a185bc0907c
Reuben <dsouzareuben79@gmail.com>
2025-10-27 20:04:07 +0530

    home screen changes

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -34,7 +34,7 @@ class HomeScreen extends StatelessWidget {
                   const SizedBox(height: 20),
                   SizedBox(
                     width: 450,
-                    height: 450,
+                    height: 451,
                     child: Image.asset(ImageAssets.house),
                   ),
                 ],
